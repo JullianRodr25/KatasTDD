d2647d7dbd9c563316a8e88882722800e3995042
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-17 15:48:17 -0500

    🔴  Si_EnvioVacio_Debe_RetornarCero

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace StringCalculator;
+
+public class StringCalculator
+{
+    [Fact]
+    public void Si_EnvioVacio_Debe_RetornarCero()
+    {
+        var resultado = retornaNumeroCero("");
+        resultado.Should().Be(0);
+    }
+
+    private object retornaNumeroCero(string empty)
+    {
+        throw new NotImplementedException();
+    }
+}
--- StringCalculator/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StringCalculator;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
