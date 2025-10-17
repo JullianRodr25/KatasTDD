5221df13c49eb16686d22339c925816dd10c7585
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-17 16:00:04 -0500

    🔴  Si_EnvioUnNumero_Debe_RetornarElMismoNumero

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -11,6 +11,14 @@ public class StringCalculator
         resultado.Should().Be(0);
     }
 
+    [Fact]
+    public void Si_EnvioUnNumero_Debe_RetornarElMismoNumero()
+    {
+        var resultado = retornaNumeroCero("4");
+        resultado.Should().Be(4);
+        
+    }
+
     private object retornaNumeroCero(string valor)
     {
         return valor == "" ? 0 : valor;
--- StringCalculator/obj/Debug/net9.0/StringCalculator.AssemblyInfo.cs
+++ StringCalculator/obj/Debug/net9.0/StringCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a8dc936738df48fa62dec5fb4789c4200e2705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b5e6cb249a1dd80749935c54eda0ce238c958a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
