ca7cfcfd2f69972fe5443cb821286d977c474d66
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-19 00:36:36 -0500

    🔴  Si_EnviamosUnaPalabra_Debe_RetornarVerdadero

---
+++ RadarPalindromes/RadarPalindromes/RadarPalindromes.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace RadarPalindromes;
+
+public class RadarPalindromes
+{
+    [Fact]
+    public void Si_EnviamosUnaPalabra_Debe_RetornarVerdadero()
+    {
+       var resultado = RetornarVerdadero("Palabra");
+       resultado.Should().Be(true);
+    }
+
+    private object RetornarVerdadero(string palabra)
+    {
+        return false;
+    }
+}
