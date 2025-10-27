6ece46fdd54bd9ef30b8ab1ac8436817883c9a28
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-10-27 11:00:00 -0500

    🔴 Si_EnvioCualquierContraseña_DebeRetornarVerdadero

--- Tennis/Tennis/obj/Debug/net9.0/Tennis.AssemblyInfo.cs
+++ Tennis/Tennis/obj/Debug/net9.0/Tennis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tennis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de0f8f6b6891d66322a85c5211e8be37a27e68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5748883ac53ae875cc4885b463c1f79cc3dc2cb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tennis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tennis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ValidacionDeContrasena/ValidacionDeContrasena/UnitTest1.cs
+++ ValidacionDeContrasena/ValidacionDeContrasena/UnitTest1.cs
@@ -1,9 +1,26 @@
-﻿namespace ValidacionDeContrasena;
+﻿using FluentAssertions;
+
+namespace ValidacionDeContrasena;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Si_EnvioCualquierContraseña_DebeRetornarVerdadero()
     {
+        // Arrange
+        
+        // Act
+        var esValida = validaContrasena("Julian1234");
+        // Assert
+
+        esValida.Should().Be(true);
+
+
+
+    }
+
+    private object validaContrasena(string julian1234)
+    {
+        throw new NotImplementedException();
     }
 }
